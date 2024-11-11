49cbecce5b2a814d8807686e61162d1284f0980b
Dowon <ks2515@naver.com>
2024-11-11 16:25:24 +0900

    feat: add __version__

--- src/jrcf/__init__.py
+++ src/jrcf/__init__.py
@@ -4,8 +4,12 @@ import jpype
 import jpype.imports
 from jpype.types import *  # type: ignore [reportWildcardImportFromLibrary] # noqa: F403
 
+from .__version__ import __version__
+
 here = Path(__file__).parent
 libs = here / "lib" / "*"
 
 jpype.addClassPath(str(libs))
 jpype.startJVM(convertStrings=False)
+
+__all__ = ["__version__"]
